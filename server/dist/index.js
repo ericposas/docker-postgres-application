8b02c2235b801e05aa6d4bcd80eaeb3850818040
Eric Posas <ericposas@outlook.com>
2021-05-24 14:08:19 -0500

    added precommit hook and starting implementing pg db integration

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,10 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
+require('dotenv').config();
+// db
+const postgresCreds = {
+    user: process.env.PGSQL_USER,
+    host: process.env.PGSQL_USER,
+    database: process.env.PGSQL_DB,
+    password: process.env.PGSQL_PASSWORD,
+    port: process.env.PGSQL_PORT
+};
+// express
 const app = express_1.default();
 const port = 3000;
 app.use('/', express_1.default.static(path_1.default.join(__dirname, '../../frontend/dist')));
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
+    console.log(postgresCreds);
 });
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,18 @@
+import pg from 'pg';
 import path from 'path';
 import express from 'express';
+require('dotenv').config();
 
+// db
+const postgresCreds = {
+  user: process.env.PGSQL_USER,
+  host: process.env.PGSQL_USER,
+  database: process.env.PGSQL_DB,
+  password: process.env.PGSQL_PASSWORD,
+  port: process.env.PGSQL_PORT
+};
+
+// express
 const app = express();
 const port: number = 3000;
 
@@ -8,4 +20,5 @@ app.use('/', express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
+  console.log(postgresCreds);
 });
