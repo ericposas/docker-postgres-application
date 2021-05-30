43587055265f8a8fbaae41f8f9102a617c3c78f7
Eric Posas <ericposas@outlook.com>
2021-05-30 15:55:56 -0500

    some style changes

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -64,70 +64,80 @@ const App = ({}) => {
   return (
     <>
       <div
-      className="
-      text-center max-w-xs
-      mx-auto mt-4
-      "
+      className=""
       >
-        <motion.img
-        {...motionBouncy}
+        <div
         className="
-        filter drop-shadow-xl
-        rounded-md select-none
+        pt-6 pb-12 rounded
+        text-center max-w-7xl
+        mx-auto mt-4 bg-yellow-700
         "
-        src="https://i.redd.it/k1bvnwiox7l31.jpg">
-        </motion.img>
-        <br />
-        <motion.button
-        {...motionButton}
-        onClick={() => addDag(setDags)}
-        className="btn-blue ml-4"
         >
-          Add a Dag
-        </motion.button>
-        <motion.button
-        {...motionButton}
-        onClick={() => deleteDags(setDags)}
-        className="btn-blue-md"
-        >Remove All Dags
-        </motion.button>
+          <motion.img
+          {...motionBouncy}
+          className="
+          filter drop-shadow-xl
+          rounded-md select-none w-1/4 m-auto
+          "
+          src="https://i.redd.it/k1bvnwiox7l31.jpg">
+          </motion.img>
+          <br />
+          <motion.button
+          {...motionButton}
+          onClick={() => addDag(setDags)}
+          className="btn-blue ml-4"
+          >
+            Add a Dag
+          </motion.button>
+          <motion.button
+          {...motionButton}
+          onClick={() => deleteDags(setDags)}
+          className="btn-blue-md"
+          >Remove All Dags
+          </motion.button>
+        {
+          dags.length > 0 ?
+          <table
+          className="
+          rounded
+          mt-6 mx-auto
+          table-auto w-8/12
+          "
+          >
+            <thead>
+              <tr className="
+              text-left font-bold
+              bg-yellow-600 text-white
+            ">
+                <th>Name</th>
+                <th>Breed</th>
+              </tr>
+            </thead>
+            <tbody
+            className="
+            text-left
+            bg-yellow-200
+            text-yellow-800
+            ">
+              {dags &&
+                dags.map((dag) => (
+                  <motion.tr
+                  key={dag.id}
+                  className="p-10"
+                  {...motionTRow}
+                  >
+                    <td>{dag.name}</td>
+                    <td>{dag.breed}</td>
+                  </motion.tr>
+                ))}
+            </tbody>
+          </table>
+          : <div
+            className="text-center mt-10 text-lg text-white"
+            >No daggies!</div>
+        }
+        </div>
       </div>
-      {
-        dags.length > 0 ?
-        <table
-        className="
-        rounded
-        mt-6 mx-auto
-        table-auto w-8/12
-        "
-        >
-          <thead>
-            <tr className="
-            text-left font-bold
-          bg-yellow-500
-          text-white">
-              <th>Name</th>
-              <th>Breed</th>
-            </tr>
-          </thead>
-          <tbody className="bg-yellow-200 text-yellow-800">
-            {dags &&
-              dags.map((dag) => (
-                <motion.tr
-                key={dag.id}
-                className="p-10"
-                {...motionTRow}
-                >
-                  <td>{dag.name}</td>
-                  <td>{dag.breed}</td>
-                </motion.tr>
-              ))}
-          </tbody>
-        </table>
-        : <div
-          className="text-center mt-10 text-lg text-white"
-          >No daggies!</div>
-      }
     </>
   );
 };
